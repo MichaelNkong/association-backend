3a4694db8fc23d6e10591c8c14a17024af4ea4b3
MichaelNkong <nkongho.ndip@gmail.com>
2025-02-14 18:45:54 +0100

    Initial commit

---
+++ association-backend/src/main/java/com/culture/association_backend/AssociationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.culture.association_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociationBackendApplication.class, args);
+	}
+
+}
